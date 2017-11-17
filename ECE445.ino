627aa6660a557b56803af03bfce102ec6a15923e
Lyu Nuochen <lnc0519@gmail.com>
2017-11-17 02:21:30 -0600

    add start up letter

--- ECE445.ino
+++ ECE445.ino
@@ -4,9 +4,9 @@
 #define CIRCLE 1
 #define CROSS 2
 
-int Max7219_pinCLK = 10;
+int Max7219_pinCLK = 11;
 int Max7219_pinCS = 9;
-int Max7219_pinDIN = 8;
+int Max7219_pinDIN = 12; 
 
 /*Patterns Dictionary*/
 unsigned char pattern[3][8] = {
@@ -59,6 +59,7 @@ unsigned char disp1[38][8]={
 {0x0,0x41,0x22,0x14,0x8,0x14,0x22,0x41},//X
 {0x41,0x22,0x14,0x8,0x8,0x8,0x8,0x8},//Y
 {0x0,0x7F,0x2,0x4,0x8,0x10,0x20,0x7F},//Z
+{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} // empty 
 };
 
 /* General byte wise write */
@@ -69,10 +70,14 @@ void Write_Max7219_byte(unsigned char DATA)
         for(i=8;i>=1;i--)
         {     
              // Serial.println(digitalRead(5));
-             digitalWrite(Max7219_pinCLK,LOW);
-             digitalWrite(Max7219_pinDIN,DATA&0x80);// Extracting a bit data
+
+             //digitalWrite(Max7219_pinCLK,LOW);
+             PORTB = PORTB&0xf7;
+             //digitalWrite(Max7219_pinDIN,DATA&0x80);// Extracting a bit data ,looks a bit werid but works 
+             PORTB = (PORTB&0xef)|((DATA&0x80)>>3);
              DATA = DATA<<1;
-             digitalWrite(Max7219_pinCLK,HIGH);
+             PORTB = PORTB|0x08;
+             //digitalWrite(Max7219_pinCLK,HIGH);
         }  
 }
 
@@ -82,7 +87,15 @@ void Write_Max7219(unsigned char address,unsigned char dat,unsigned char LED_pos
        PORTD = (PORTD&0x0f)|(LED_position<<4);
        Write_Max7219_byte(address);           //address，code of LED
        Write_Max7219_byte(dat);               //data，figure on LED
-       PORTD=(PORTD&0x0f)|0xf0;
+       PORTD=(PORTD|0xf0);
+}
+
+void Write_Max7219_letter(unsigned char address,unsigned char dat,unsigned char letter_position)
+{
+       PORTB = (PORTB&0xfc);//|(letter_position&0x03);
+       Write_Max7219_byte(address);           //address，code of LED
+       Write_Max7219_byte(dat);               //data，figure on LED
+       PORTB=(PORTB|0x03);
 }
 
 /* Init LED chip */
@@ -96,6 +109,13 @@ void Init_MAX7219(void)
           Write_Max7219(0x0b, 0x07,i);       //scanlimit；8 LEDs
           Write_Max7219(0x0c, 0x01,i);       //power-down mode：0，normal mode：1
           Write_Max7219(0x0f, 0x00,i);       //test display：1；EOT，display：0
+       }
+       for (i=0;i<4;i++){
+          Write_Max7219_letter(0x09, 0x00,i);       //decoding ：BCD
+          Write_Max7219_letter(0x0a, 0x03,i);       //brightness 
+          Write_Max7219_letter(0x0b, 0x07,i);       //scanlimit；8 LEDs
+          Write_Max7219_letter(0x0c, 0x01,i);       //power-down mode：0，normal mode：1
+          Write_Max7219_letter(0x0f, 0x00,i);       //test display：1；EOT，display：0
        }
  
 }
@@ -120,12 +140,14 @@ void setup()
 {
       //Initial Serial      
       //declear CS DIN
-      pinMode(Max7219_pinCLK,OUTPUT);
-      pinMode(Max7219_pinDIN,OUTPUT);
+      //pinMode(Max7219_pinCLK,OUTPUT);
+      //pinMode(Max7219_pinDIN,OUTPUT);
 
       //declear PIN 0 to 7
-      DDRD = B11111111;
+      DDRD = B11110000;
 
+      //declear PIN 8 to 13
+      DDRB = B11111011;
       //init 3*3 LED module
       Init_MAX7219();
 
@@ -133,15 +155,41 @@ void setup()
       LED_empty();
 }
 
+
 /* Set single LED */
 void LED_set(int LED_position, int LED_pattern){
      int i;
      
      for(i=1; i<9; i++){
-        Write_Max7219(i,pattern[LED_pattern][i-1],LED_position);
+        Write_Max7219(i,disp1[LED_pattern][i-1],LED_position);
      }
      
      LED_status_set(LED_position, LED_pattern);
+}
+void LED_set_9_letter(char* array1){
+   int i = 0 ;
+   int letter; 
+   for(i=0;i<9;i++){
+    if(array1[i]>=97){
+      letter = array1[i]-97 + 10;
+    }
+    else if (array1[i]>=48){
+      letter = array1[i] - 48 ;
+    }
+    //error reading
+    else if(array1[i]==32)
+    letter =  36;
+    else 
+    letter = 14;
+    LED_set(i,letter);
+   }
+}
+
+void Letter_set(int letter_position,int Letter_pattern){
+     int i;
+     for(i=1; i<9; i++){
+        Write_Max7219_letter(i,disp1[Letter_pattern][i-1],letter_position);
+     }
 }
 
 /* Refresh LED */
@@ -160,17 +208,86 @@ void LED_refresh(){
 void LED_status_set(int LED_position, int LED_pattern) {
      LED_status[LED_position] = LED_pattern;
 }
+int get_press_led (){
+  Letter_set(0,5);
+  unsigned char mask = 0x0f; 
+  int input;
+  input = (PIND&0x0f)^mask;
+  while (input==0);
+  return input-1;
+}
+void pressure_led_set(int LED_pattern){
 
+
+  unsigned char input ;
+    input= get_press_led ();
+    LED_set(input,LED_pattern);
+}
+//we can change this to wait for anything being pressed
+void wait_start_button(){
+  unsigned char a =PINB;
+  while(a==PINB){
+    a =PINB;
+  }
+}
+int choose_mode(){
+  int pressed_button=get_press_led ();
+  if(pressed_button==1)
+  return 0;
+  else if (pressed_button==7)
+  return 1;
+  else return 2;
+}
+void single_player(){
+  LED_set_9_letter("singleply");
+  delay(3000);
+  return;
+}
+void double_player(){
+  LED_set_9_letter("doubleply");
+  delay(3000);
+  return;
+}
+void not_correct_mode(){
+  LED_set_9_letter("chooseagn");
+  delay(3000);
+}
+void game_start(){
+  LED_set_9_letter("gamestart");
+  delay(3000);
+  choosemod:
+  LED_set_9_letter("choosemod");
+  delay (3000);
+  LED_set_9_letter("1plo r2pl");
+  delay (3000);
+  LED_set_9_letter(" 1     2 ");
+  int mode = choose_mode();
+  if(mode==0){
+  single_player();  
+  }
+  else if (mode==1){
+  double_player();  
+  }
+  else{
+  not_correct_mode();
+  goto choosemod ;
+  }
+}
 /*
  * Game Main Body
  */
 void loop()
 { 
-    LED_set(6,0);
-    LED_refresh();
-    LED_Game_Print();
-    LED_set(3,0);
-    LED_refresh();
-    LED_Game_Print();
+  wait_start_button();
+  Letter_set(0,3);
+  game_start();
+
+
+
+//    LED_refresh();
+//    LED_Game_Print();
+//    LED_set(3,0);
+//    LED_refresh();
+//    LED_Game_Print();
     while(1){};
 }
